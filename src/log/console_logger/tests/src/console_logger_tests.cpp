13dc2566ab0208fa607570c2d4b168a73913d843
Kolyunya <oleynikovny@mail.ru>
2013-12-11 06:19:29 +0000

    commit

--- src/log/console_logger/tests/src/console_logger_tests.cpp
+++ src/log/console_logger/tests/src/console_logger_tests.cpp
@@ -1,29 +1,28 @@
 ﻿#include <gtest/gtest.h>
 #include <log/console_logger.hpp>
 
-
-
 TEST ( console_logger , does_not_throw_while_construction )
 {
-
     std::console_logger(false,false);
     ASSERT_TRUE(true);
-
 }
 
-TEST ( console_logger , does_not_throw_while_logging )
+TEST ( console_logger , does_not_throw_while_logging_000 )
 {
-
-    const std::abstract_logger& logger = std::console_logger(false,false);
-    logger.log("");
+    const std::abstract_logger& logger = std::console_logger();
+    logger.log("Hello world");
     ASSERT_TRUE(true);
+}
 
+TEST ( console_logger , does_not_throw_while_logging_001 )
+{
+    const std::abstract_logger& logger = std::console_logger(true,false);
+    logger.log("Hello world");
+    ASSERT_TRUE(true);
 }
 
 int main ( int argc , char** argv )
 {
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-
 }
