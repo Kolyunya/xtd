524663bc8afec9eabe5853ed23d377a6b282840e
Kolyunya <oleynikovny@mail.ru>
2014-01-22 16:36:39 +0400

    Parameter pack library update

--- src/utility/parameter_pack/src/parameter_pack.hpp
+++ src/utility/parameter_pack/src/parameter_pack.hpp
@@ -6,96 +6,121 @@
 namespace std
 {
 
-    #pragma region RETRIEVE_NTH_PARAMETER_TYPE
+    struct parameter_pack
+    {
 
-        template < int parameter_position , typename... parameters_types >
-        struct type_of
-        {
+        // REGION HELPER_TYPES
 
-        };
+            public:
 
-        template < typename first_parameter_type , typename... other_parameters_types >
-        struct type_of < 0 , first_parameter_type , other_parameters_types... >
-        {
-            using type = first_parameter_type;
-        };
+                struct null_type
+                {
 
-        template < int parameter_position , typename first_parameter_type , typename... other_parameters_types >
-        struct type_of < parameter_position , first_parameter_type , other_parameters_types... >
-        {
-            using type = typename type_of < parameter_position - 1 , other_parameters_types... >::type;
-        };
+                };
 
-        template < typename... parameters >
-        struct type_of_first
-        {
-            using type = typename type_of < 0 , parameters... >::type;
-        };
-
-        template < typename... parameters >
-        struct type_of_last
-        {
-            using type = typename type_of < sizeof...(parameters) - 1 , parameters... >::type;
-        };
-
-    #pragma endregion
+        // ENDREGION
 
 
 
-    #pragma region RETRIEVE_NTH_PARAMETER_VALUE
-    #pragma endregion
+        // REGION RETRIEVE_NTH_PARAMETER_TYPE
+
+            private:
+
+                template < int parameter_position , typename... parameters_types >
+                struct type_of
+                {
+                    using type = null_type;
+                };
+
+                template < typename first_parameter_type , typename... other_parameters_types >
+                struct type_of < 0 , first_parameter_type , other_parameters_types... >
+                {
+                    using type = first_parameter_type;
+                };
+
+                template < int parameter_position , typename first_parameter_type , typename... other_parameters_types >
+                struct type_of < parameter_position , first_parameter_type , other_parameters_types... >
+                {
+                    using type = typename type_of<parameter_position-1,other_parameters_types...>::type;
+                };
+
+            public:
+
+                template < int parameter_position , typename... parameters_types >
+                struct type_of_nth
+                {
+                    using type = typename type_of < parameter_position , parameters_types... >::type;
+                };
+
+                template < typename... parameters_types >
+                struct type_of_first
+                {
+                    using type = typename type_of_nth < 0 , parameters_types... >::type;
+                };
+
+                template < typename... parameters_types >
+                struct type_of_last
+                {
+                    using type = typename type_of_nth < sizeof...(parameters_types) - 1 , parameters_types... >::type;
+                };
+
+        // ENDREGION
 
 
 
-    #pragma region DETECTION_OF_HOMO_AND_HETERO_GENEOUS_PARAMETER_PACKS
+        // REGION RETRIEVE_NTH_PARAMETER_VALUE
 
-        template < typename... parameters_types >
-        auto parameter_pack_is_homogeneous ( void )
-            ->
-                typename std::enable_if<(sizeof...(parameters_types) < 2),bool>::type
-        {
-            return true;
-        }
+            public:
 
-        template < typename first_parameter_type , typename second_parameter_type , typename... other_parameters_types >
-        bool parameter_pack_is_homogeneous ( void )
-        {
-
-            std::size_t first_parameter_type_hash_code = typeid(first_parameter_type).hash_code();
-            std::size_t second_parameter_type_hash_code = typeid(second_parameter_type).hash_code();
-            bool first_and_second_parameters_types_hash_codes_are_equal = first_parameter_type_hash_code == second_parameter_type_hash_code;
-
-            if ( first_and_second_parameters_types_hash_codes_are_equal == false )
-            {
-                return false;
-            }
-
-            std::size_t parameters_pack_size = sizeof...(other_parameters_types);
-            if ( parameters_pack_size == 0 )
-            {
-                return true;
-            }
-
-            return parameter_pack_is_homogeneous<second_parameter_type,other_parameters_types...>();
-
-        }
-
-        template < typename... parameters_types >
-        auto parameter_pack_is_heterogeneous ( void )
-            ->
-                typename std::enable_if<(sizeof...(parameters_types) < 2),bool>::type
-        {
-            return ! parameter_pack_is_homogeneous<parameters_types...>();
-        }
-
-        template < typename first_parameter_type , typename second_parameter_type , typename... other_parameters_types >
-        bool parameter_pack_is_heterogeneous ( void )
-        {
-            return ! parameter_pack_is_homogeneous<first_parameter_type,second_parameter_type,other_parameters_types...>();
-        }
+        // ENDREGION
 
 
-    #pragma endregion
+
+        // REGION DETECTION_OF_HOMO_AND_HETERO_GENEOUS_PARAMETER_PACKS
+
+        public:
+
+                template < typename... parameters_types >
+                struct is_homogeneous
+                {
+                    static const bool value = true;
+                    using type = null_type;
+                };
+
+                template < typename parameter_type >
+                struct is_homogeneous < parameter_type >
+                {
+                    static const bool value = true;
+                    using type = parameter_type;
+                };
+
+                template < typename first_parameter_type , typename... other_parameters_types >
+                struct is_homogeneous < first_parameter_type , other_parameters_types... >
+                {
+                    private:
+                        using other_parameters_type = typename is_homogeneous<other_parameters_types...>::type;
+                    public:
+                        static const bool value = std::is_same<first_parameter_type,other_parameters_type>::value;
+                        using type = typename std::conditional<value,first_parameter_type,null_type>::type;
+                };
+
+                template < typename... parameters_types >
+                struct is_heterogeneous
+                {
+                    static const bool value = ! is_homogeneous<parameters_types...>::value;
+                };
+
+        // ENDREGION
+
+
+
+        // REGION RETRIEVE_HOMOGENOUS_PARAMETER_PACK_TYPE
+
+            public:
+
+        // ENDREGION
+
+    };
 
 }
 
--- src/utility/parameter_pack/tests/src/parameter_pack_tests.cpp
+++ src/utility/parameter_pack/tests/src/parameter_pack_tests.cpp
@@ -4,8 +4,8 @@
 
 TEST ( first_parameter_pack_element_type , is_retrieved_correctly_000 )
 {
-    using first_parameter_type = typename std::type_of_first<std::string,bool,int,double>::type;
-    std::size_t first_parameter_type_hash_expected = typeid(std::string).hash_code();
+    using first_parameter_type = typename std::parameter_pack::type_of_first<>::type;
+    std::size_t first_parameter_type_hash_expected = typeid(std::parameter_pack::null_type).hash_code();
     std::size_t first_parameter_type_hash_actual = typeid(first_parameter_type).hash_code();
     bool first_parameter_is_recognized_correctly = first_parameter_type_hash_expected == first_parameter_type_hash_actual;
     ASSERT_TRUE(first_parameter_is_recognized_correctly);
@@ -13,7 +13,16 @@ TEST ( first_parameter_pack_element_type , is_retrieved_correctly_000 )
 
 TEST ( first_parameter_pack_element_type , is_retrieved_correctly_001 )
 {
-    using first_parameter_type = typename std::type_of_first<double,std::string,bool,int>::type;
+    using first_parameter_type = typename std::parameter_pack::type_of_first<std::string,bool,int,double>::type;
+    std::size_t first_parameter_type_hash_expected = typeid(std::string).hash_code();
+    std::size_t first_parameter_type_hash_actual = typeid(first_parameter_type).hash_code();
+    bool first_parameter_is_recognized_correctly = first_parameter_type_hash_expected == first_parameter_type_hash_actual;
+    ASSERT_TRUE(first_parameter_is_recognized_correctly);
+}
+
+TEST ( first_parameter_pack_element_type , is_retrieved_correctly_002 )
+{
+    using first_parameter_type = typename std::parameter_pack::type_of_first<double,std::string,bool,int>::type;
     std::size_t first_parameter_type_hash_expected = typeid(double).hash_code();
     std::size_t first_parameter_type_hash_actual = typeid(first_parameter_type).hash_code();
     bool first_parameter_is_recognized_correctly = first_parameter_type_hash_expected == first_parameter_type_hash_actual;
@@ -22,7 +31,7 @@ TEST ( first_parameter_pack_element_type , is_retrieved_correctly_001 )
 
 TEST ( last_parameter_pack_element_type , is_retrieved_correctly_000 )
 {
-    using first_parameter_type = typename std::type_of_last<std::string,bool,int,double>::type;
+    using first_parameter_type = typename std::parameter_pack::type_of_last<std::string,bool,int,double>::type;
     std::size_t first_parameter_type_hash_expected = typeid(double).hash_code();
     std::size_t first_parameter_type_hash_actual = typeid(first_parameter_type).hash_code();
     bool first_parameter_is_recognized_correctly = first_parameter_type_hash_expected == first_parameter_type_hash_actual;
@@ -31,7 +40,7 @@ TEST ( last_parameter_pack_element_type , is_retrieved_correctly_000 )
 
 TEST ( last_parameter_pack_element_type , is_retrieved_correctly_001 )
 {
-    using first_parameter_type = typename std::type_of_last<double,std::string,bool,int>::type;
+    using first_parameter_type = typename std::parameter_pack::type_of_last<double,std::string,bool,int>::type;
     std::size_t first_parameter_type_hash_expected = typeid(int).hash_code();
     std::size_t first_parameter_type_hash_actual = typeid(first_parameter_type).hash_code();
     bool first_parameter_is_recognized_correctly = first_parameter_type_hash_expected == first_parameter_type_hash_actual;
@@ -40,55 +49,61 @@ TEST ( last_parameter_pack_element_type , is_retrieved_correctly_001 )
 
 TEST ( empty_parameter_pack , is_homogeneous )
 {
-    bool empty_parameter_pack_is_homogeneous = std::parameter_pack_is_homogeneous<>();
+    bool empty_parameter_pack_is_homogeneous = std::parameter_pack::is_homogeneous<>::value;
+    using empty_parameter_pack_type = std::parameter_pack::is_homogeneous<>::type;
+    bool empty_parameter_pack_type_is_null_type = std::is_same<std::parameter_pack::null_type,empty_parameter_pack_type>::value;
     ASSERT_TRUE(empty_parameter_pack_is_homogeneous);
+    ASSERT_TRUE(empty_parameter_pack_type_is_null_type);
 }
 
 TEST ( empty_parameter_pack , is_not_heterogeneous )
 {
-    bool empty_parameter_pack_is_heterogeneous = std::parameter_pack_is_heterogeneous<>();
+    bool empty_parameter_pack_is_heterogeneous = std::parameter_pack::is_heterogeneous<>::value;
     ASSERT_FALSE(empty_parameter_pack_is_heterogeneous);
 }
 
 TEST ( parameter_pack_consisting_of_one_integer , is_homogeneous )
 {
-    bool parameter_pack_consisting_of_one_integer_is_homogeneous = std::parameter_pack_is_homogeneous<int>();
-    ASSERT_TRUE(parameter_pack_consisting_of_one_integer_is_homogeneous);
+    bool pack_is_homogeneous = std::parameter_pack::is_homogeneous<int>::value;
+    using pack_type = std::parameter_pack::is_homogeneous<int>::type;
+    bool pack_type_is_int = std::is_same<int,pack_type>::value;
+    ASSERT_TRUE(pack_is_homogeneous);
+    ASSERT_TRUE(pack_type_is_int);
 }
 
 TEST ( parameter_pack_consisting_of_one_integer , is_not_heterogeneous )
 {
-    bool parameter_pack_consisting_of_one_integer_is_heterogeneous = std::parameter_pack_is_heterogeneous<int>();
+    bool parameter_pack_consisting_of_one_integer_is_heterogeneous = std::parameter_pack::is_heterogeneous<int>::value;
     ASSERT_FALSE(parameter_pack_consisting_of_one_integer_is_heterogeneous);
 }
 
 TEST ( parameter_pack_consisting_of_two_integers , is_homogeneous )
 {
-    bool parameter_pack_consisting_of_two_integers_is_homogeneous = std::parameter_pack_is_homogeneous<int,int>();
+    bool parameter_pack_consisting_of_two_integers_is_homogeneous = std::parameter_pack::is_homogeneous<int,int>::value;
     ASSERT_TRUE(parameter_pack_consisting_of_two_integers_is_homogeneous);
 }
 
 TEST ( parameter_pack_consisting_of_two_integers , is_not_heterogeneous )
 {
-    bool parameter_pack_consisting_of_two_integers_is_heterogeneous = std::parameter_pack_is_heterogeneous<int,int>();
+    bool parameter_pack_consisting_of_two_integers_is_heterogeneous = std::parameter_pack::is_heterogeneous<int,int>::value;
     ASSERT_FALSE(parameter_pack_consisting_of_two_integers_is_heterogeneous);
 }
 
 TEST ( parameter_pack_consisting_of_integer_and_float , is_not_homogeneous )
 {
-    bool parameter_pack_consisting_of_integer_and_float_is_not_homogeneous = std::parameter_pack_is_homogeneous<int,float>();
+    bool parameter_pack_consisting_of_integer_and_float_is_not_homogeneous = std::parameter_pack::is_homogeneous<int,float>::value;
     ASSERT_FALSE(parameter_pack_consisting_of_integer_and_float_is_not_homogeneous);
 }
 
 TEST ( parameter_pack_consisting_of_three_integers , is_homogeneous )
 {
-    bool parameter_pack_consisting_of_three_integers_is_homogeneous = std::parameter_pack_is_homogeneous<int,int,int>();
+    bool parameter_pack_consisting_of_three_integers_is_homogeneous = std::parameter_pack::is_homogeneous<int,int,int>::value;
     ASSERT_TRUE(parameter_pack_consisting_of_three_integers_is_homogeneous);
 }
 
 TEST ( parameter_pack_consisting_of_three_integers_and_boolean , is_heterogeneous )
 {
-    bool parameter_pack_consisting_of_three_integers_and_boolean_is_heterogeneous = std::parameter_pack_is_heterogeneous<int,int,int,bool>();
+    bool parameter_pack_consisting_of_three_integers_and_boolean_is_heterogeneous = std::parameter_pack::is_heterogeneous<int,int,int,bool>::value;
     ASSERT_TRUE(parameter_pack_consisting_of_three_integers_and_boolean_is_heterogeneous);
 }
 
