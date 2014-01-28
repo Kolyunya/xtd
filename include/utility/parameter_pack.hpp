be3cddc0abbdb78c7a7e17689defc249ac7a278c
Kolyunya <oleynikovny@mail.ru>
2014-01-28 17:51:19 +0400

    Parameter pack library update

--- include/utility/parameter_pack.hpp
+++ include/utility/parameter_pack.hpp
@@ -4,6 +4,6 @@
 #include "./../../src/utility/parameter_pack/parameter_pack_element_type/src/parameter_pack_element_type.hpp"
 #include "./../../src/utility/parameter_pack/parameter_pack_element_value/src/parameter_pack_element_value.hpp"
 #include "./../../src/utility/parameter_pack/parameter_pack_homogeneity/src/parameter_pack_homogeneity.hpp"
-#include "./../../src/utility/parameter_pack/parameter_pack_for_each/src/parameter_pack_for_each.hpp"
+//#include "./../../src/utility/parameter_pack/parameter_pack_for_each/src/parameter_pack_for_each.hpp"
 
 #endif // _INCLUDE_STD_PARAMETER_PACK_HPP_
