e0c2e19428859d54c2c2155ec8223d44640aa1dd
Kolyunya <oleynikovny@mail.ru>
2014-01-28 17:38:43 +0400

    parameter pack update

--- src/utility/parameter_pack/parameter_pack_for_each/src/parameter_pack_for_each.hpp
+++ src/utility/parameter_pack/parameter_pack_for_each/src/parameter_pack_for_each.hpp
@@ -7,6 +7,22 @@ namespace std
     namespace pp
     {
 
+        template < typename type >
+        using parameter_processor = void(*)(type);
+
+        template < void(*processor)(int) , typename... parameters_types >
+        void for_each ( parameters_types... parameters )
+        {
+
+        }
+
+        template < void(*processor)(int) , typename first_parameter_type , typename... other_parameters_types >
+        void for_each ( first_parameter_type first_parameter , other_parameters_types... other_parameters )
+        {
+            processor(first_parameter);
+            for_each<other_parameters_types...>(processor,other_parameters...);
+        }
+
     }
 
 }
--- src/utility/parameter_pack/parameter_pack_for_each/tests/src/parameter_pack_for_each_tests.cpp
+++ src/utility/parameter_pack/parameter_pack_for_each/tests/src/parameter_pack_for_each_tests.cpp
@@ -1,5 +1,16 @@
 #include <gtest/gtest.h>
 #include <utility/parameter_pack.hpp>
+#include <iostream>
+
+void printer ( int integer )
+{
+    std::cout << integer << std::endl;
+}
+
+TEST ( foo , bar )
+{
+    std::pp::for_each<printer,int,int,int>(4,2,42);
+}
 
 int main ( int argc , char** argv )
 {
