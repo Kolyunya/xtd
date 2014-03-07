c384b1083e383bdf375469088385fad7edf1f8c0
Kolyunya <oleynikovny@mail.ru>
2014-03-07 17:12:15 +0400

    commit

--- src/cmath/random/src/random.hpp
+++ src/cmath/random/src/random.hpp
@@ -1,11 +1,29 @@
-#ifndef _RANDOM_HPP_
-#define _RANDOM_HPP_
+//  xstd - extension of the C++ standard library
+//  Copyright (C) 2013 Oleynikov Nikolay
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Author email: OleynikovNY@mail.ru
+
+#ifndef _XSTD_RANDOM_HPP_
+#define _XSTD_RANDOM_HPP_
 
 #include <stdexcept>    // std::range_error
 #include <cstdlib>      // std::rand
 #include <ctime>        // std::time
 
-namespace std
+namespace xstd
 {
 
     namespace random
@@ -13,7 +31,7 @@ namespace std
 
         inline void     randomize ( void );
 
-        inline int      get_int ( const int minimum , const int maximum );
+        inline int      get_int ( int minimum , int maximum );
 
         inline bool     coin_flip ( float win_rate_percent = 50 );
 
@@ -24,4 +42,4 @@ namespace std
 // Include implementation file
 #include "random.ipp"
 
-#endif  //  _RANDOM_HPP_
+#endif  //  _XSTD_RANDOM_HPP_
--- src/cmath/random/tests/src/random_tests.cpp
+++ src/cmath/random/tests/src/random_tests.cpp
@@ -1,13 +1,29 @@
+//  xstd - extension of the C++ standard library
+//  Copyright (C) 2013 Oleynikov Nikolay
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Author email: OleynikovNY@mail.ru
+
 #include <gtest/gtest.h>
 #include <stdlib.h>         // abs
 #include <cmath.hpp>
 
-
-
 TEST ( get_int_1_2 , returns_1_or_2 )
 {
 
-    int random_integer = std::random::get_int(1,2);
+    int random_integer = xstd::random::get_int(1,2);
 
     ASSERT_TRUE
     (
@@ -21,7 +37,7 @@ TEST ( get_int_1_2 , returns_1_or_2 )
 TEST ( get_int_minus_5_minus_3 , returns_minus_5_or_minus_4_or_minus_3 )
 {
 
-    int random_integer = std::random::get_int(-5,-3);
+    int random_integer = xstd::random::get_int(-5,-3);
 
     ASSERT_TRUE
     (
@@ -37,14 +53,14 @@ TEST ( get_int_minus_5_minus_3 , returns_minus_5_or_minus_4_or_minus_3 )
 TEST ( get_int_2_1 , throws_std_range_error )
 {
 
-    ASSERT_THROW(std::random::get_int(2,1),std::range_error);
+    ASSERT_THROW(xstd::random::get_int(2,1),std::range_error);
 
 }
 
 TEST ( coin_flip , returns_true_or_false )
 {
 
-    bool coin_flip = std::random::coin_flip();
+    bool coin_flip = xstd::random::coin_flip();
 
     ASSERT_TRUE
     (
@@ -58,7 +74,7 @@ TEST ( coin_flip , returns_true_or_false )
 TEST ( coin_flip_10 , returns_true_or_false )
 {
 
-    bool coin_flip = std::random::coin_flip(10);
+    bool coin_flip = xstd::random::coin_flip(10);
 
     ASSERT_TRUE
     (
@@ -79,7 +95,7 @@ TEST ( coin_flip_10 , returns_true_in_10_percents_of_cases )
     for ( int key = 0 ; key < 10000 ; key++ )
     {
 
-        if ( std::random::coin_flip(10) == true )
+        if ( xstd::random::coin_flip(10) == true )
         {
 
             wins++;
@@ -94,9 +110,7 @@ TEST ( coin_flip_10 , returns_true_in_10_percents_of_cases )
 
 int main ( int argc , char** argv )
 {
-
-    std::random::randomize();
+    xstd::random::randomize();
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-
 }
