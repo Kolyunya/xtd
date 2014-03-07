330efd24f97d87a627f15f5926f85d82f056fccf
Kolyunya <oleynikovny@mail.ru>
2014-03-07 17:20:57 +0400

    commit

--- src/functional/functor/functor/src/functor.hpp
+++ src/functional/functor/functor/src/functor.hpp
@@ -1,52 +1,51 @@
+//  xstd - extension of the C++ standard library
+//  Copyright (C) 2013 Oleynikov Nikolay
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Author email: OleynikovNY@mail.ru
+
 #ifndef _XSTD_FUNCTOR_HPP_
 #define _XSTD_FUNCTOR_HPP_
 
-//#include <functional/abstract_functor.hpp>
+#include <functional/abstract_functor.hpp>
 
-namespace std
+namespace xstd
 {
-
-    template <typename... arguments_type>
-    class abstract_functor
-    {
-        public:
-            virtual         ~abstract_functor ( void ) = default;
-            virtual void    operator() ( arguments_type... arguments ) const = 0;
-            virtual void    invoke ( arguments_type... arguments ) const = 0;
-    };
 
     template <typename object_type , typename... arguments_type>
     class functor
         :
             public abstract_functor<arguments_type...>
     {
+
         public:
-            explicit        functor ( object_type* object_ptr , void(object_type::*method_ptr)(arguments_type...) )
-            {
-                if
-                (
-                    object_ptr == nullptr
-                        ||
-                    method_ptr == nullptr
-                )
-                {
-                    throw -1;
-                }
-                this->object_ptr = object_ptr;
-                this->method_ptr = method_ptr;
-            }
-            virtual         ~functor ( void ) noexcept override = default;
-            virtual void    operator() ( arguments_type... arguments ) const override final
-            {
-                this->invoke(arguments...);
-            }
-            virtual void    invoke ( arguments_type... arguments ) const override final
-            {
-                ((this->object_ptr)->*(this->method_ptr))(arguments...);
-            }
-            void            (object_type::*method_ptr)(arguments_type...);
-            object_type*    object_ptr;
+
+            inline explicit         functor ( object_type* object_ptr , void(object_type::*method_ptr)(arguments_type...) );
+
+            inline virtual          ~functor ( void ) noexcept override = default;
+
+            inline virtual void     operator() ( arguments_type... arguments ) const override final;
+
+            inline virtual void     invoke ( arguments_type... arguments ) const override final;
+
+            void                    (object_type::*method_ptr)(arguments_type...);
+
+            object_type*            object_ptr;
+
     };
+
 }
 
 // Include implementation file
--- src/functional/functor/functor/src/functor.tpp
+++ src/functional/functor/functor/src/functor.tpp
@@ -3,29 +3,30 @@ namespace std
 
             template <typename object_type , typename parameter_type>
             functor<object_type,parameter_type>::functor ( object_type* object_ptr , void(object_type::*method_ptr)(parameter_type*) )
-                :
-                    object_ptr ( object_ptr ),
-                    method_ptr ( method_ptr )
     {
-
+        if
+        (
+            object_ptr == nullptr
+                ||
+            method_ptr == nullptr
+        )
+        {
+            throw -1;
+        }
+        this->object_ptr = object_ptr;
+        this->method_ptr = method_ptr;
     }
 
             template <typename object_type , typename parameter_type>
     void    functor<object_type,parameter_type>::operator() ( void* parameter_void_ptr ) const
     {
-
-        parameter_type* parameter_ptr = reinterpret_cast<parameter_type*>(parameter_void_ptr);
-        ( this->object_ptr ->* this->method_ptr ) ( parameter_ptr );
-
+        this->invoke(arguments...);
     }
 
             template <typename object_type , typename parameter_type>
     void    functor<object_type,parameter_type>::invoke ( void* parameter_void_ptr ) const
     {
-
-        parameter_type* parameter_ptr = reinterpret_cast<parameter_type*>(parameter_void_ptr);
-        ( *this ) ( parameter_ptr );
-
+        ((this->object_ptr)->*(this->method_ptr))(arguments...);
     }
 
 }
