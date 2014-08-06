baf1dfd64ef5f8c694b8c2c15831cfbfe8878404
Kolyunya <oleynikovny@mail.ru>
2014-08-06 23:38:31 +0400

    Merge in of specification from master branch

---
+++ documentation/html/search/functions_66.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['file_5flogger',['file_logger',['../classxstd_1_1file__logger.html#a6e43dc8871b62e45550ce6470dcf5477',1,'xstd::file_logger']]],
+  ['for_5feach',['for_each',['../namespacexstd_1_1pp.html#a55868e0a2c2057ea2ba4ee9a938b8f07',1,'xstd::pp::for_each(parameters_types...)'],['../namespacexstd_1_1pp.html#aa29a2aab754d68ff365898122abea2fe',1,'xstd::pp::for_each(first_parameter_type first_parameter, other_parameters_types...other_parameters)']]],
+  ['functor',['functor',['../classxstd_1_1functor.html#aed2d510d4df5f6bb33d06370fbfa5fb8',1,'xstd::functor']]]
+];
---
+++ search/classes_63.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['console_5flogger',['console_logger',['../classxstd_1_1console__logger.html',1,'xstd']]],
+  ['coutmt_5fsingleton',['coutmt_singleton',['../classxstd_1_1coutmt__singleton.html',1,'xstd']]]
+];
