ea94e17389b9cd824687e12cb761bd1cc9c29c7e
Kolyunya <oleynikovny@mail.ru>
2014-08-07 10:57:55 +0400

    Merge in of specification from master branch

---
+++ search/functions_61.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['abstract_5flogger',['abstract_logger',['../classxstd_1_1abstract__logger.html#a57d17bcf4d3260fab819d98643671c78',1,'xstd::abstract_logger']]],
+  ['add_5flistener',['add_listener',['../classxstd_1_1event.html#a6d7320f608d4f7b072b62a5e66ea8d62',1,'xstd::event::add_listener(free_listener free_listener_instance)'],['../classxstd_1_1event.html#aa58880f86e0e2e5653df91692f221604',1,'xstd::event::add_listener(object_type *object_ptr, void(object_type::*method_ptr)(data_type...))']]],
+  ['add_5ftimer',['add_timer',['../classxstd_1_1chrono_1_1timer__manager.html#ae97c0dba8154bace5af93caae4948c8c',1,'xstd::chrono::timer_manager']]],
+  ['append_5ffile_5fcontents',['append_file_contents',['../namespacexstd_1_1fs.html#a748a84e128ba84f9c78fc644dc2b7614',1,'xstd::fs']]]
+];
