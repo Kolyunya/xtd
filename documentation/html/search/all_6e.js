a05dcc4974e3bbbb1660625f2808d39ad0df006f
Kolyunya <oleynikovny@mail.ru>
2014-08-07 15:48:25 +0400

    commit

--- documentation/html/search/all_6e.js
+++ documentation/html/search/all_6e.js
@@ -1,11 +1,8 @@
 var searchData=
 [
-  ['next_5ftick_5ftime_5fpoint',['next_tick_time_point',['../classxstd_1_1chrono_1_1timer.html#a51e85ae6c2a43cef074e5ff3e03620f9',1,'xstd::chrono::timer']]],
   ['nth_5ftype',['nth_type',['../structxstd_1_1pp_1_1nth__type.html',1,'xstd::pp']]],
   ['nth_5ftype_5fhelper',['nth_type_helper',['../structxstd_1_1pp_1_1nth__type__helper.html',1,'xstd::pp']]],
   ['nth_5ftype_5fhelper_3c_200_2c_20first_5fparameter_5ftype_2c_20other_5fparameters_5ftypes_2e_2e_2e_20_3e',['nth_type_helper&lt; 0, first_parameter_type, other_parameters_types... &gt;',['../structxstd_1_1pp_1_1nth__type__helper_3_010_00_01first__parameter__type_00_01other__parameters__types_8_8_8_01_4.html',1,'xstd::pp']]],
   ['nth_5ftype_5fhelper_3c_20parameter_5fid_2c_20first_5fparameter_5ftype_2c_20other_5fparameters_5ftypes_2e_2e_2e_20_3e',['nth_type_helper&lt; parameter_id, first_parameter_type, other_parameters_types... &gt;',['../structxstd_1_1pp_1_1nth__type__helper_3_01parameter__id_00_01first__parameter__type_00_01other__parameters__types_8_8_8_01_4.html',1,'xstd::pp']]],
-  ['nth_5fvalue',['nth_value',['../namespacexstd_1_1pp.html#a64f472b507404fee4aad504684c76040',1,'xstd::pp::nth_value(void)-&gt; null_type'],['../namespacexstd_1_1pp.html#a6cd873bf9694830db9dba2301cc223a5',1,'xstd::pp::nth_value(first_parameter_type &amp;&amp;first_parameter, other_parameters_types &amp;&amp;...)-&gt; typename std'],['../namespacexstd_1_1pp.html#a64a0d91fb344668e87c3d7052c3dfbb5',1,'xstd::pp::nth_value(first_parameter_type &amp;&amp;, other_parameters_types &amp;&amp;...other_parameters)-&gt; typename std']]],
-  ['null_5ftype',['null_type',['../structxstd_1_1pp_1_1null__type.html',1,'xstd::pp']]],
-  ['number_5fto_5fstring',['number_to_string',['../namespacextd_1_1str.html#a84c0f8285134f075fde7fbb91ee174ef',1,'xtd::str::number_to_string(int source_number)'],['../namespacextd_1_1str.html#a485b63dcd6cac4d8b45712f634a2aaed',1,'xtd::str::number_to_string(unsigned int source_number)'],['../namespacextd_1_1str.html#ae520ea89838af34abca58612baf0ee7d',1,'xtd::str::number_to_string(float source_number)']]]
+  ['null_5ftype',['null_type',['../structxstd_1_1pp_1_1null__type.html',1,'xstd::pp']]]
 ];
