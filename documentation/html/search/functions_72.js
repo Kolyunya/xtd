cd1dbf13c794fb7b8bcc4f0dd4f982470638c4c6
Kolyunya <oleynikovny@mail.ru>
2014-08-07 16:04:17 +0400

    commit

--- documentation/html/search/functions_72.js
+++ documentation/html/search/functions_72.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['raii_5fthread',['raii_thread',['../classxstd_1_1raii__thread.html#a8bc39a85a55f9becb860febe87f9175c',1,'xstd::raii_thread']]],
-  ['raii_5fthread_5fbase',['raii_thread_base',['../classxstd_1_1raii__thread__base.html#a8f6cf744a47869c0e28a5e8a127c172b',1,'xstd::raii_thread_base']]],
+  ['raii_5fthread_5fbase',['raii_thread_base',['../classxstd_1_1raii__thread__base.html#a8f6cf744a47869c0e28a5e8a127c172b',1,'xstd::raii_thread_base::raii_thread_base(std::function&lt; void()&gt; client_routine)'],['../classxstd_1_1raii__thread__base.html#abcff331991c469d9749a533324a1347c',1,'xstd::raii_thread_base::raii_thread_base(const raii_thread_base &amp;rhs)']]],
   ['raii_5fthread_5fmanual',['raii_thread_manual',['../classxstd_1_1raii__thread__manual.html#a7a6c9e815d104ba820b3deaa5eef4000',1,'xstd::raii_thread_manual']]],
   ['randomize',['randomize',['../namespacexstd_1_1random.html#aebd951891ee8cc82c7a61482ad3dabc6',1,'xstd::random']]],
   ['remove_5flistener',['remove_listener',['../classxstd_1_1event.html#a19e40e6d6af2d565ef741ecfbfb2fda7',1,'xstd::event::remove_listener(free_listener free_listener_instance)'],['../classxstd_1_1event.html#a2ecf3e9a39708db0c28281dbb32d12da',1,'xstd::event::remove_listener(object_type *object_ptr, void(object_type::*method_ptr)(data_type...))']]],
