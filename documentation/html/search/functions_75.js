86aba0179d38bbb1bd9e349bbeb23b83e825e2ab
Kolyunya <oleynikovny@mail.ru>
2014-08-07 08:53:36 +0400

    co

--- documentation/html/search/functions_75.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['update_5fnext_5ftick_5ftime_5fpoint',['update_next_tick_time_point',['../classxstd_1_1chrono_1_1timer.html#aef7b4094eebf22027987e81993859488',1,'xstd::chrono::timer']]]
-];
--- search/functions_76.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['validate_5fcoordinate_5fid',['validate_coordinate_id',['../classxstd_1_1point.html#a9c83662efe0927f03cb9cb1cfab796cd',1,'xstd::point']]],
-  ['validate_5fdimension',['validate_dimension',['../classxstd_1_1point.html#a1d9e30c142c79d1c21bdb271c81dabb5',1,'xstd::point']]],
-  ['validate_5fdimension_5fmatch',['validate_dimension_match',['../classxstd_1_1point.html#a5e32aa4388cfc0d9b9b3df56c5d8bd91',1,'xstd::point::validate_dimension_match(const coordinates_initializer_list &amp;coordinates_object) const '],['../classxstd_1_1point.html#a15edf9d511dcd8d27c98e77b18033573',1,'xstd::point::validate_dimension_match(const point&lt; _type, _dimension &gt; &amp;) const ']]]
-];
