a404973a6b72f4a1929d17528ee77c67923e2d7e
Kolyunya <oleynikovny@mail.ru>
2014-08-07 16:52:49 +0400

    commit

--- documentation/html/search/files_61.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['abstract_5ffunctor_2ehpp',['abstract_functor.hpp',['../abstract__functor_8hpp.html',1,'']]],
-  ['abstract_5flogger_2ehpp',['abstract_logger.hpp',['../abstract__logger_8hpp.html',1,'']]],
-  ['abstract_5flogger_2eipp',['abstract_logger.ipp',['../abstract__logger_8ipp.html',1,'']]]
-];
--- documentation/html/search/files_6c.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['logger_5fclient_2ehpp',['logger_client.hpp',['../logger__client_8hpp.html',1,'']]],
-  ['logger_5fclient_2eipp',['logger_client.ipp',['../logger__client_8ipp.html',1,'']]],
-  ['logger_5fclient_5ftests_2ecpp',['logger_client_tests.cpp',['../logger__client__tests_8cpp.html',1,'']]]
-];
