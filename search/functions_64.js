27f6953f8c373698ca655e84adec5a625a33d9e4
Kolyunya <oleynikovny@mail.ru>
2014-08-07 10:52:28 +0400

    Merge in of specification from master branch

--- search/functions_64.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['deinitialize_5froutine',['deinitialize_routine',['../classxstd_1_1raii__thread__base.html#ae423d8023eb8c3bed9b4b0f11c055c2d',1,'xstd::raii_thread_base']]],
-  ['dispatch',['dispatch',['../classxstd_1_1event.html#a60e1b1471f7c50f764fcb8388ca56b47',1,'xstd::event']]],
-  ['dispatch_5ffree_5flisteners',['dispatch_free_listeners',['../classxstd_1_1event.html#ac3a4cf6f6d139f3ab31cd3ca1ebb42e0',1,'xstd::event']]],
-  ['dispatch_5ffunctor_5flisteners',['dispatch_functor_listeners',['../classxstd_1_1event.html#a3c82e7e9e05c4c44e65b32db7090bd3c',1,'xstd::event']]]
-];
