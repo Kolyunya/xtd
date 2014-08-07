62c06cd29fb73ed2b471fb47f7582ba6c5e4ca11
Kolyunya <oleynikovny@mail.ru>
2014-08-07 15:14:33 +0400

    String update

--- src/string/string_util/src/string_util.hpp
+++ src/string/string_util/src/string_util.hpp
@@ -52,9 +52,9 @@ namespace xtd
          *  @param [in] source_string A string representation of a number.
          *  @param [in] number_base A base of a number.
          *  @returns A numeric representation of a number.
-         *  @throws std::runtime_error Throws if `source_string` does not represent a valid_number.
-         *                            Also throws if `source_string` represents a number which is out of range of `long int`.
-         *  @no_throw_guarantee Strong no-throw guarantee.
+         *  @no_throw_guarantee Strong.
+         *  @throws std::runtime_error Thrown if `source_string` does not represent a valid_number.
+         *                             Also thrown if `source_string` represents a number which is out of range of `long int`.
          *
          *  @details Converts a number represented by a `source_string` to a `long int`.
          *           Treats a `source_string` as a numeral with a base of `number_base`.
