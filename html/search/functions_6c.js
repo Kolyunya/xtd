994197b9a99ec1c4f013bf515ccf8ca1eb3a030d
Kolyunya <oleynikovny@mail.ru>
2014-08-07 16:54:05 +0400

    commit

--- html/search/functions_6c.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['last_5fvalue',['last_value',['../namespacexstd_1_1pp.html#a0fbe1362608c9cc1906521a66ef46026',1,'xstd::pp']]],
-  ['log',['log',['../classxstd_1_1abstract__logger.html#ad470970ac4726355f3423017f434ace8',1,'xstd::abstract_logger::log()'],['../classxstd_1_1logger__client.html#a7169793d2c5512150d611ae47e0c6b74',1,'xstd::logger_client::log()']]],
-  ['log_5fdata',['log_data',['../classxstd_1_1abstract__logger.html#a8cf4f5fd1490b54680060022a2f8ac91',1,'xstd::abstract_logger::log_data()'],['../classxstd_1_1console__logger.html#ad53afba0752b8cca7e5ccde101ec0954',1,'xstd::console_logger::log_data()'],['../classxstd_1_1file__logger.html#a806b0a7c20bad624afbddd2346312ca6',1,'xstd::file_logger::log_data()']]],
-  ['log_5fnew_5fline',['log_new_line',['../classxstd_1_1abstract__logger.html#a106b39652dd0d1efb32d8ad52ae6555c',1,'xstd::abstract_logger']]],
-  ['log_5ftime_5fstamp',['log_time_stamp',['../classxstd_1_1abstract__logger.html#a8bed4dfa79399f58146700adfd1ac06f',1,'xstd::abstract_logger']]],
-  ['logger_5fclient',['logger_client',['../classxstd_1_1logger__client.html#ae366adb742d038fa072522500d798ce8',1,'xstd::logger_client']]]
-];
