ee377894645b1e52d200d75ad0cd38ea50733388
Kolyunya <oleynikovny@mail.ru>
2014-08-07 15:59:23 +0400

    commit

--- documentation/html/search/all_68.js
+++ documentation/html/search/all_68.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['has_5flistener',['has_listener',['../classxstd_1_1event.html#a28d29a9104d4e8c3986376b2ffca0a48',1,'xstd::event::has_listener(free_listener free_listener_instance) const '],['../classxstd_1_1event.html#acbfdc5b60687237a2b42bc6e7d00d4a4',1,'xstd::event::has_listener(object_type *object_ptr, void(object_type::*method_ptr)(data_type...)) const ']]],
-  ['has_5ftimer',['has_timer',['../classxstd_1_1chrono_1_1timer__manager.html#a88bb334bc67435a46b08a99dc2d45ed1',1,'xstd::chrono::timer_manager']]]
+  ['has_5flistener',['has_listener',['../classxstd_1_1event.html#a28d29a9104d4e8c3986376b2ffca0a48',1,'xstd::event::has_listener(free_listener free_listener_instance) const '],['../classxstd_1_1event.html#acbfdc5b60687237a2b42bc6e7d00d4a4',1,'xstd::event::has_listener(object_type *object_ptr, void(object_type::*method_ptr)(data_type...)) const ']]]
 ];
