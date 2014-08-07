7907a3fc9d9e3e9f58f009ef145e8837d44fa9a9
Kolyunya <oleynikovny@mail.ru>
2014-08-07 09:03:36 +0400

    Merge in of specification from master branch

---
+++ search/classes_70.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['point',['point',['../classxstd_1_1point.html',1,'xstd']]],
+  ['printer',['printer',['../structprinter.html',1,'']]]
+];
---
+++ search/namespaces_78.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['chrono',['chrono',['../namespacexstd_1_1chrono.html',1,'xstd']]],
+  ['fs',['fs',['../namespacexstd_1_1fs.html',1,'xstd']]],
+  ['pp',['pp',['../namespacexstd_1_1pp.html',1,'xstd']]],
+  ['random',['random',['../namespacexstd_1_1random.html',1,'xstd']]],
+  ['xstd',['xstd',['../namespacexstd.html',1,'']]]
+];
