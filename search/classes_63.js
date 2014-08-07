31fbc9f840b2219cb68dbf7407a41bf5b5df87f8
Kolyunya <oleynikovny@mail.ru>
2014-08-07 15:30:34 +0400

    Merge in of documentation from master branch

--- search/classes_63.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['console_5flogger',['console_logger',['../classxstd_1_1console__logger.html',1,'xstd']]],
-  ['coutmt_5fsingleton',['coutmt_singleton',['../classxstd_1_1coutmt__singleton.html',1,'xstd']]]
-];
--- search/related_6f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['operator_3c_3c',['operator&lt;&lt;',['../classxstd_1_1point.html#a26f9c85c349535d8d87c0b2b4dda134e',1,'xstd::point::operator&lt;&lt;()'],['../classxstd_1_1coutmt__singleton.html#aae5812b8fa2d66eaaa921e864e1077d4',1,'xstd::coutmt_singleton::operator&lt;&lt;()'],['../classxstd_1_1coutmt__singleton.html#a75138493a4c47f61c28762df29d5af97',1,'xstd::coutmt_singleton::operator&lt;&lt;()'],['../classxstd_1_1coutmt__singleton.html#af343264ff6f5ba75b8f6eab378c55638',1,'xstd::coutmt_singleton::operator&lt;&lt;()'],['../classxstd_1_1coutmt__singleton.html#af794d8d7f2993af42e84429e5f5b4c9a',1,'xstd::coutmt_singleton::operator&lt;&lt;()']]]
-];
