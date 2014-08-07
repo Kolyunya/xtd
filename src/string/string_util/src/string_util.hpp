7d1f59d5df00292aee24b5363d2ec43d67b83985
Kolyunya <oleynikovny@mail.ru>
2014-08-07 13:07:05 +0400

    commit

--- src/string/string_util/src/string_util.hpp
+++ src/string/string_util/src/string_util.hpp
@@ -44,7 +44,7 @@ namespace xstd
 
 
      /**
-     *  @brief =qweqwdsafdsdfssdfddfsdfdssfdsgdsd=====++++++++++Converts a number from string to numeric representation.
+     *  @brief =qweqwdsafdsdfssdfdsdfdfsdfdssfdsgdsd=====++++++++++Converts a number from string to numeric representation.
      *
      *  @param [in] source_string A string representation of a number.
      *  @param [in] number_base A base of a number.
