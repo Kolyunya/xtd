88258e60418ed5e3389bdcc34508266bf390b4fb
Kolyunya <oleynikovny@mail.ru>
2014-08-07 15:48:25 +0400

    Merge in of documentation from master branch

--- search/typedefs_70.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['point_5ff2',['point_f2',['../namespacexstd.html#aa26f7d45c70ace7cd0722a5d5d89dcd8',1,'xstd']]],
-  ['point_5ff3',['point_f3',['../namespacexstd.html#ac9dcb9387fb1c6f4f82773a54bfa5843',1,'xstd']]],
-  ['point_5fi2',['point_i2',['../namespacexstd.html#a05f630b2b093cec28fcdd96aca897f75',1,'xstd']]],
-  ['point_5fi3',['point_i3',['../namespacexstd.html#a28fb4f569d80e07173722c8cc3465f81',1,'xstd']]]
-];
