c71ba421e6c23790245b6314c44a09c26101761f
Kolyunya <oleynikovny@mail.ru>
2014-08-12 11:34:39 +0400

    Library documentation updated.

---
+++ sources/string/string_utils/build/out/debug/.deps/out/debug/obj.target/tests/string_utils.o.d
@@ -0,0 +1,55 @@
+cmd_out/debug/obj.target/string_utils_tests/../tests/string_utils.o := g++  -I/home/kolyunya/projects/xtd/include -I/home/kolyunya/projects/gtest/include  -std=c++11 -Wall -Wextra -Werror -pedantic -pedantic-errors -Wno-unused-variable -O0 -g  -MMD -MF out/debug/.deps/out/debug/obj.target/string_utils_tests/../tests/string_utils.o.d.raw  -c -o out/debug/obj.target/string_utils_tests/../tests/string_utils.o ../tests/string_utils.cpp
+out/debug/obj.target/string_utils_tests/../tests/string_utils.o: \
+ ../tests/string_utils.cpp \
+ /home/kolyunya/projects/gtest/include/gtest/gtest.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-internal.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-port.h \
+ /home/kolyunya/projects/gtest/include/gtest/gtest-message.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-string.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-filepath.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-type-util.h \
+ /home/kolyunya/projects/gtest/include/gtest/gtest-death-test.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-death-test-internal.h \
+ /home/kolyunya/projects/gtest/include/gtest/gtest-param-test.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-param-util.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-linked_ptr.h \
+ /home/kolyunya/projects/gtest/include/gtest/gtest-printers.h \
+ /home/kolyunya/projects/gtest/include/gtest/internal/gtest-param-util-generated.h \
+ /home/kolyunya/projects/gtest/include/gtest/gtest_prod.h \
+ /home/kolyunya/projects/gtest/include/gtest/gtest-test-part.h \
+ /home/kolyunya/projects/gtest/include/gtest/gtest-typed-test.h \
+ /home/kolyunya/projects/gtest/include/gtest/gtest_pred_impl.h \
+ /home/kolyunya/projects/xtd/include/xtd/string.hpp \
+ /home/kolyunya/projects/xtd/include/xtd/string/string_utils.hpp \
+ /home/kolyunya/projects/xtd/include/xtd/string/./../../../sources/string/string_utils/sources/string_utils.hpp \
+ /home/kolyunya/projects/xtd/include/xtd/string/./../../../sources/string/string_utils/sources/string_utils.tpp \
+ ../tests/to_long_long.hpp ../tests/to_long.hpp ../tests/to_int.hpp \
+ ../tests/to_double.hpp ../tests/to_float.hpp
+../tests/string_utils.cpp:
+/home/kolyunya/projects/gtest/include/gtest/gtest.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-internal.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-port.h:
+/home/kolyunya/projects/gtest/include/gtest/gtest-message.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-string.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-filepath.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-type-util.h:
+/home/kolyunya/projects/gtest/include/gtest/gtest-death-test.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-death-test-internal.h:
+/home/kolyunya/projects/gtest/include/gtest/gtest-param-test.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-param-util.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-linked_ptr.h:
+/home/kolyunya/projects/gtest/include/gtest/gtest-printers.h:
+/home/kolyunya/projects/gtest/include/gtest/internal/gtest-param-util-generated.h:
+/home/kolyunya/projects/gtest/include/gtest/gtest_prod.h:
+/home/kolyunya/projects/gtest/include/gtest/gtest-test-part.h:
+/home/kolyunya/projects/gtest/include/gtest/gtest-typed-test.h:
+/home/kolyunya/projects/gtest/include/gtest/gtest_pred_impl.h:
+/home/kolyunya/projects/xtd/include/xtd/string.hpp:
+/home/kolyunya/projects/xtd/include/xtd/string/string_utils.hpp:
+/home/kolyunya/projects/xtd/include/xtd/string/./../../../sources/string/string_utils/sources/string_utils.hpp:
+/home/kolyunya/projects/xtd/include/xtd/string/./../../../sources/string/string_utils/sources/string_utils.tpp:
+../tests/to_long_long.hpp:
+../tests/to_long.hpp:
+../tests/to_int.hpp:
+../tests/to_double.hpp:
+../tests/to_float.hpp:
