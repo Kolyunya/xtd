5ceaa9e4d6b967a2a2b3e1eafa5782d1bc6e6e64
Kolyunya <oleynikovny@mail.ru>
2014-08-12 16:14:38 +0400

    Library documentation updated.

--- search/files_63.js
+++ search/files_63.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['coutmt_2ecpp',['coutmt.cpp',['../sources_2coutmt_8cpp.html',1,'']]],
   ['coutmt_2ecpp',['coutmt.cpp',['../tests_2sources_2coutmt_8cpp.html',1,'']]],
-  ['coutmt_2ehpp',['coutmt.hpp',['../coutmt_8hpp.html',1,'']]]
+  ['coutmt_2ehpp',['coutmt.hpp',['../coutmt_8hpp.html',1,'']]],
+  ['coutmt_2etpp',['coutmt.tpp',['../coutmt_8tpp.html',1,'']]]
 ];
