2ca6050938be495244ef1f52f0b4fc15f5463982
Kolyunya <oleynikovny@mail.ru>
2014-08-12 23:28:56 +0400

    Library documentation updated.

--- build/out/debug/.deps/out/debug/obj.target/sources/random/random_utils/sources/random_utils.o.d
+++
@@ -1,6 +0,0 @@
-cmd_out/debug/obj.target/random_utils/../sources/random/random_utils/sources/random_utils.o := g++  -I/home/kolyunya/projects/xtd/include  -std=c++11 -Wall -Wextra -Werror -pedantic -pedantic-errors -Wno-unused-variable -O0 -g  -MMD -MF out/debug/.deps/out/debug/obj.target/random_utils/../sources/random/random_utils/sources/random_utils.o.d.raw  -c -o out/debug/obj.target/random_utils/../sources/random/random_utils/sources/random_utils.o ../sources/random/random_utils/sources/random_utils.cpp
-out/debug/obj.target/random_utils/../sources/random/random_utils/sources/random_utils.o: \
- ../sources/random/random_utils/sources/random_utils.cpp \
- ../sources/random/random_utils/sources/random_utils.hpp
-../sources/random/random_utils/sources/random_utils.cpp:
-../sources/random/random_utils/sources/random_utils.hpp:
--- build/out/debug/.deps/out/debug/sources/io/coutmt/build/libcoutmt.a.d
+++
@@ -1,1 +0,0 @@
-cmd_out/debug/obj.target/../sources/io/coutmt/build/libcoutmt.a := rm -f out/debug/obj.target/../sources/io/coutmt/build/libcoutmt.a && ar crsT out/debug/obj.target/../sources/io/coutmt/build/libcoutmt.a out/debug/obj.target/coutmt/../sources/io/coutmt/sources/coutmt.o
