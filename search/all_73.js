b8955ca0a100828117b490bde2544683e75b5fc2
Kolyunya <oleynikovny@mail.ru>
2014-08-12 20:34:09 +0400

    Library documentation updated.

--- search/all_73.js
+++ search/all_73.js
@@ -6,9 +6,8 @@ var searchData=
   ['start_5fthread_5fif_5fneeded',['start_thread_if_needed',['../classxtd_1_1chrono_1_1timer__manager.html#a3656535d20fe04e6234b314c2c27b7d2',1,'xtd::chrono::timer_manager']]],
   ['stop',['stop',['../classxtd_1_1chrono_1_1timer.html#ad2bea72fb6c07586fab5e19311548082',1,'xtd::chrono::timer']]],
   ['stop_5fthread_5fif_5fneeded',['stop_thread_if_needed',['../classxtd_1_1chrono_1_1timer__manager.html#a9114d54e8ea13005525b16209b08bb37',1,'xtd::chrono::timer_manager']]],
-  ['str',['str',['../namespacestr.html',1,'']]],
-  ['string_5futils_2ecpp',['string_utils.cpp',['../sources_2string__utils_8cpp.html',1,'']]],
   ['string_5futils_2ecpp',['string_utils.cpp',['../tests_2sources_2string__utils_8cpp.html',1,'']]],
+  ['string_5futils_2ecpp',['string_utils.cpp',['../sources_2string__utils_8cpp.html',1,'']]],
   ['string_5futils_2ehpp',['string_utils.hpp',['../string__utils_8hpp.html',1,'']]],
   ['string_5futils_2etpp',['string_utils.tpp',['../string__utils_8tpp.html',1,'']]]
 ];
