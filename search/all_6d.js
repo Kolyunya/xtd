6d7bdad3d7c2f2aa3bf7f9b0827928f7bc03acb5
Kolyunya <oleynikovny@mail.ru>
2014-08-12 11:56:44 +0400

    Library documentation updated.

--- search/all_6d.js
+++ search/all_6d.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['main',['main',['../tests_2memory__utils_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;memory_utils.cpp'],['../tests_2string__utils_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;string_utils.cpp']]],
-  ['memory_5futils_2ecpp',['memory_utils.cpp',['../tests_2memory__utils_8cpp.html',1,'']]],
+  ['mem',['mem',['../namespacemem.html',1,'']]],
   ['memory_5futils_2ecpp',['memory_utils.cpp',['../sources_2memory__utils_8cpp.html',1,'']]],
+  ['memory_5futils_2ecpp',['memory_utils.cpp',['../tests_2memory__utils_8cpp.html',1,'']]],
   ['memory_5futils_2ehpp',['memory_utils.hpp',['../memory__utils_8hpp.html',1,'']]],
   ['memory_5futils_2etpp',['memory_utils.tpp',['../memory__utils_8tpp.html',1,'']]]
 ];
