b2fd8cf53167499d939288ab9da442fc02117761
Kolyunya <oleynikovny@mail.ru>
2014-08-12 11:30:55 +0400

    Library documentation updated.

--- search/variables_65.js
+++ search/variables_65.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['error_5fnumber_5fexceeds_5ftype_5flimits',['error_number_exceeds_type_limits',['../namespacextd_1_1str.html#a9a2f12e0b4f16b6917d74c1217b810b8',1,'xtd::str']]],
-  ['error_5fstring_5fto_5fnumber_5fconversion_5ffailed',['error_string_to_number_conversion_failed',['../namespacextd_1_1str.html#acc372bee2f761f902368f1689576e267',1,'xtd::str']]]
+  ['error_5fnumber_5fexceeds_5ftype_5flimits',['error_number_exceeds_type_limits',['../namespacextd_1_1str.html#a8c731a55e9b1235e437a61b728354794',1,'xtd::str']]],
+  ['error_5fstring_5fto_5fnumber_5fconversion_5ffailed',['error_string_to_number_conversion_failed',['../namespacextd_1_1str.html#a8d0b649c019e83b1f02367b8454143a7',1,'xtd::str']]]
 ];
